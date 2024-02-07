81806b97ff2668d9e855a3d33af030b6e335ebcc
joseuilton <joseuilton.siqueira@gmail.com>
2024-02-07 15:49:41 -0300

    feat: add get poll route

---
+++ src/http/routes/get-poll.ts
@@ -0,0 +1,29 @@
+import { FastifyInstance } from "fastify";
+import z from "zod";
+import { prisma } from "../../lib/prisma";
+
+export async function getPoll(app: FastifyInstance){
+  app.get("/polls/:pollId", async(request, reply) => {
+    const getPollParams = z.object({
+      pollId: z.string()
+    });
+
+    const { pollId } = getPollParams.parse(request.params);
+
+    const poll = await prisma.poll.findUnique({
+      where: {
+        id: pollId,
+      },
+      include: {
+        options: {
+          select: {
+            id: true,
+            title: true
+          }
+        }
+      }
+    });
+
+    return reply.send({ poll });
+  });
+}
