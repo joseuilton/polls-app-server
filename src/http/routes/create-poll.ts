4bf23a06350bf54faa8a6e20f20e8c01f35ca81e
joseuilton <joseuilton.siqueira@gmail.com>
2024-02-07 12:33:35 -0300

    refactor: moving route create poll for a file

---
+++ src/http/routes/create-poll.ts
@@ -0,0 +1,22 @@
+import { FastifyInstance } from "fastify";
+import z from "zod";
+
+import { prisma } from "../../lib/prisma";
+
+export async function createPoll(app: FastifyInstance) {
+  app.post("/polls", async(request, reply) => {
+    const createPollBody = z.object({
+      title: z.string()
+    });
+
+    const { title } = createPollBody.parse(request.body);
+
+    const poll = await prisma.poll.create({
+      data: {
+        title
+      }
+    });
+
+    return reply.status(201).send({ pollId: poll.id });
+  });
+}
